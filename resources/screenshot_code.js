57781e1666d6eda14fb51ca1618a55f3156468cc
roeniss(문성혁) <roeniss.milk@kakaocorp.com>
2020-12-16 02:00:02 +0900

    add 1280x800 screenshot

--- resources/screenshot_code.js
+++ resources/screenshot_code.js
@@ -4,5 +4,5 @@ copy!
 ...and paste?
 "https://unicodes.net/%ED%95%9C%EA%B5%AD%E6%97%A5%E6%9C%AC%5E*~[-+/wow!" 😡 😡 😡
 
-what about paste with ^Decode URI by Click^ ?
+what about paste with ^Decode URL by Click^ ?
 "https://unicodes.net/한국日本^*~[-+/wow!" 😇 🤗 👍
